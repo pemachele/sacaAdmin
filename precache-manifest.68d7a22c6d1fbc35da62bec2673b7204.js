ddd05f73601d39505174a953eab8c61d3b870928
pemachele <pemachele96@gmail.com>
2020-08-22 04:56:54 +0300

    Updates

---
+++ precache-manifest.68d7a22c6d1fbc35da62bec2673b7204.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6466d50e11e67b0216f1422f72d0e0",
+    "url": "/sacaAdmin/index.html"
+  },
+  {
+    "revision": "759c255cbbb5ed0c0fc3",
+    "url": "/sacaAdmin/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "66714c236ba5d3d632ca",
+    "url": "/sacaAdmin/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "759c255cbbb5ed0c0fc3",
+    "url": "/sacaAdmin/static/js/2.3c8cef52.chunk.js"
+  },
+  {
+    "revision": "3e83a3071bf9f08c857214e75ad76cdf",
+    "url": "/sacaAdmin/static/js/2.3c8cef52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66714c236ba5d3d632ca",
+    "url": "/sacaAdmin/static/js/main.d197503e.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/sacaAdmin/static/js/main.d197503e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763474978a7fbc4f2249",
+    "url": "/sacaAdmin/static/js/runtime-main.4a9ac016.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/sacaAdmin/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/sacaAdmin/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/sacaAdmin/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/sacaAdmin/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/sacaAdmin/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
